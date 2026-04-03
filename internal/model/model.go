7e822ca361d0d47555f7b3b504556e71f7f7d1ed
Rodrigo Mecheri <67772460+mexirica@users.noreply.github.com>
2026-04-03 14:39:26 +0200

    feat: add manual package export functionality and related key bindings (#89)
    * feat: add manual package export functionality and related key bindings
    
    * feat: add manual error handling to package loading process

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,17 +1,18 @@
 package model
 
 type Package struct {
-	Name           string
-	Version        string
-	Size           string
-	Description    string
-	Installed      bool
-	Upgradable     bool
-	NewVersion     string
-	Section        string
-	Architecture   string
-	SecurityUpdate bool
-	Held           bool
-	Pinned         bool
-	Essential      bool
+	Name              string
+	Version           string
+	Size              string
+	Description       string
+	Installed         bool
+	Upgradable        bool
+	NewVersion        string
+	Section           string
+	Architecture      string
+	SecurityUpdate    bool
+	Held              bool
+	Pinned            bool
+	Essential         bool
+	ManuallyInstalled bool
 }
